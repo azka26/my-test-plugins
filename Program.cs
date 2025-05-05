8521c5399d3814b24ac67b16664b45dfec6a6a8b
Yurizal Rosa Andika <rosa.andika@live.com>
2025-05-05 11:42:23 +0700

    Asset Maintenance GP

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace GitHubActionExample
 {
@@ -13,6 +14,32 @@ namespace GitHubActionExample
             else
             {
                 Console.WriteLine("Tidak ada input yang diberikan.");
+            }
+
+            // Run a bash command
+            Console.WriteLine("Executing bash command...");
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "/bin/bash",
+                Arguments = "-c \"echo 'Hello from Bash'\"",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            try
+            {
+                using (Process process = Process.Start(startInfo))
+                {
+                    string output = process.StandardOutput.ReadToEnd();
+                    process.WaitForExit();
+                    Console.WriteLine($"Bash output: {output}");
+                    Console.WriteLine($"Exit code: {process.ExitCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error executing bash command: {ex.Message}");
             }
             Console.WriteLine("CURRENT DIRECTORY : " + Directory.GetCurrentDirectory());
             Console.WriteLine("GitHub Action selesai.");
