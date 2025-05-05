c44f526511cb0d347fd157ab5b711410d1033e41
Yurizal Rosa Andika <rosa.andika@live.com>
2025-05-05 17:16:06 +0700

    x

--- BuildParameter.cs
+++ BuildParameter.cs
@@ -18,9 +18,9 @@ namespace GitHubActionExample
             foreach (var arg in argsSplit)
             {
                 Console.WriteLine($"Processing argument: {arg}");
-                
+
                 var parameters = arg.Split("=", StringSplitOptions.None);
-                var parameterName = arg[0].ToString().Trim().ToLower();
+                var parameterName = parameters[0].Trim().ToLower();
                 var parameterValues = new List<string>();
                 for (int i = 0; i < parameters.Length; i++)
                 {
