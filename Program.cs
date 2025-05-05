bb52d5a1efa04fcfc7691cf854ad9243b04ae1cd
Yurizal Rosa Andika <rosa.andika@live.com>
2025-05-05 10:50:18 +0700

    test plugins

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GitHubActionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"GitHub Action menerima input: {args[0]}");
+            }
+            else
+            {
+                Console.WriteLine("Tidak ada input yang diberikan.");
+            }
+        }
+    }
+}
